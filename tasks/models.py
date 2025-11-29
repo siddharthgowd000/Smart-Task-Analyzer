9fe6c690694fe5f8ec7cc67590bf081275b43d9c
siddharthgowd000 <siddharthgowd000@gmail.com>
2025-11-29 12:37:29 +0530

    feat: Define Data Models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    due_date = models.DateField(null=True, blank=True)
+    estimated_hours = models.FloatField(default=1.0)
+    importance = models.IntegerField(default=5)
+    dependencies = models.JSONField(default=list, blank=True)
+
+    def __str__(self):
+        return self.title
